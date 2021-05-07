8a1ace02067a8eda15030e5e9666332a771bc142
maxwellburdette <maxbdevelops@gmail.com>
2021-05-07 00:49:56 -0500

    First Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Picture from "./graphics/profile.jpeg";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function handleToggle() {
+		const sec = document.querySelector(".sec");
+		sec.classList.toggle("dark");
+	}
+	return (
+		<section className="sec">
+			<div className="card">
+				<div className="toggle" onClick={handleToggle}></div>
+				<div className="content">
+					<div className="imgText">
+						<div className="imgBox">
+							<img src={Picture} alt=""></img>
+						</div>
+						<h3>
+							Maxwell Burdette
+							<br />
+							<span>Software Developer</span>
+						</h3>
+						<ul className="sci">
+							<li>
+								<a href="https://www.twitter.com">
+									<FontAwesomeIcon
+										className="icons"
+										icon={["fab", "twitter"]}
+										size="lg"
+									></FontAwesomeIcon>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com">
+									<FontAwesomeIcon
+										className="icons"
+										icon={["fab", "facebook-f"]}
+										size="lg"
+									></FontAwesomeIcon>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.linkedin.com">
+									<FontAwesomeIcon
+										className="icons"
+										icon={["fab", "linkedin-in"]}
+										size="lg"
+									></FontAwesomeIcon>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
